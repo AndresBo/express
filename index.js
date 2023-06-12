4a0d6b3826e0d09cec7faf107732099dc74e2cfa
AndresBo <anndresfborrero@gmail.com>
2023-06-12 10:18:48 +1000

    build: modified POST note method to validate that request has 'content' property

--- index.js
+++ index.js
@@ -59,13 +59,20 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
-  const maxId = notes.length > 0
+  const body = request.body
 
+  if (!body.content) {
+    return response.status(400).json({error: 'content missing'})
+  }
 
-  const note = request.body
-  note.id = maxId + 1
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
   notes = notes.concat(note)
-  console.log(note)
+
   response.json(note)
 })
 
