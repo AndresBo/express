6fff02fb1c7867ff31aa401e0964bb728ef0ecd4
AndresBo <anndresfborrero@gmail.com>
2023-06-30 19:51:19 +1000

    build: modified PORT to deploy online

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const nodemon = require('nodemon')
+const cors = require('cors')
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -81,7 +81,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
