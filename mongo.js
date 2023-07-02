1d2fca3f670bcb0691c01611284c5b8e7d33cbc0
AndresBo <anndresfborrero@gmail.com>
2023-07-02 10:45:39 +1000

    build: added comments to mongo.js and also added code that retrives data

--- mongo.js
+++ mongo.js
@@ -4,28 +4,39 @@ if (process.argv.length<3) {
     console.log('give password as argument')
     process.exit(1)
 }
-
+// take password from third argument in command line    
 const password = process.argv[2]
 
-const url = `mongodb+srv://andresb:${password}@cluster0.1isxvxb.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://andresb:${password}@cluster0.1isxvxb.mongodb.net/noteApp?retryWrites=true&w=majority`
   
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
-
+// define schema for a note 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
-
+// Note model definition. Note first parameter 'note' is singular. The name of the collection will be 'notes'.
+// Mongoose automatically names them plural when the schema refers to them in singular.
 const Note = mongoose.model('Note', noteSchema)
 
+// create a new note using Note model
 const note = new Note({
-  content: 'HTML is Easy',
+  content: 'Mogoose is an Object Data Modeling (ODM)',
   important: true,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
+// // save and close to finish executing
+// note.save().then(result => {
+//   console.log('note saved!', result)
+//   mongoose.connection.close()
+// })
+
+// to retrieve data, use 'find' method of the Note model:
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
