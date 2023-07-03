4ed3e70a8867e6916538d925f93dad34f5a7cd32
AndresBo <anndresfborrero@gmail.com>
2023-07-03 10:27:04 +1000

    build: added a request logger to console.log request

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const mongoose = require('mongoose')
 
 const Note = require('./models/note')
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(requestLogger)
 
 
 // mongoose config:
