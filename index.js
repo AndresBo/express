89255a177bf859307f1af3e87f4f8c9ffb99a0d0
AndresBo <anndresfborrero@gmail.com>
2023-07-03 10:09:55 +1000

    build: moved db configuration to its own module and modified POST method for new notes

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
+require('dotenv').config()
+
 const express = require('express')
-const cors = require('cors')
 const app = express()
+
+const cors = require('cors')
+
 const mongoose = require('mongoose')
+
+const Note = require('./models/note')
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+
 // mongoose config:
-const password = process.argv[2]
-const url = `mongodb+srv://andresb:${password}@cluster0.1isxvxb.mongodb.net/noteApp?retryWrites=true&w=majority`
+// const password = process.argv[2]
+// const url = `mongodb+srv://andresb:${password}@cluster0.1isxvxb.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+// mongoose.set('strictQuery', false)
+// mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-//
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+// // transform _id to a string and deletes _id and _v mongo versioning field
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
 
-const Note = mongoose.model('Note', noteSchema)
+//const Note = mongoose.model('Note', noteSchema)
+
 
 let notes = [
     {
@@ -52,7 +68,7 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello from express note backend</h1>')
 })
 
-// get all notes
+// get all notes from db
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -84,23 +100,22 @@ app.delete('/api/notes/:id', (request, response) => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({error: 'content missing'})
   }
 
-  const note = {
+  const note = new Note ({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
