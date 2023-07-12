92c9c15768f86f11807fdd5e01d78c7e44a76d37
AndresBo <anndresfborrero@gmail.com>
2023-07-12 12:26:09 +1000

    build: modified logger.js that outputs information about HTTP requests so it does not print to console in test mode

--- utils/logger.js
+++ utils/logger.js
@@ -2,12 +2,16 @@
 
 // function for printing normal log messages
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(...params)
+  }
 }
 
 // function for printing error messages
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 module.exports = { info, error }
