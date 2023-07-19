5a83a44ab95987c4d214960093e4b312bfc81409
AndresBo <anndresfborrero@gmail.com>
2023-07-19 11:49:47 +1000

    fix: typo in user schema transform function

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
-    delete returnedObject._v
+    delete returnedObject.__v
     // do not reveal the password
     delete returnedObject.passwordHash
   }
